533ab46f555cd7856ba27d956707c7687c69398f
mausmi_sinha <sinhatanu2001@gmail.com>
2023-02-12 17:41:19 +0530

    Glide Dependency Added

--- app/src/main/java/com/example/travel_blog/BlogDetailsActivity.java
+++ app/src/main/java/com/example/travel_blog/BlogDetailsActivity.java
@@ -3,12 +3,52 @@ package com.example.travel_blog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
 
 public class BlogDetailsActivity extends AppCompatActivity {
+    public static final String IMAGE_URL ="https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg";
+    public static final String AVATAR_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_details);
+
+        ImageView IMG1 = findViewById(R.id.imageView2);
+
+        Glide.with(this)
+                .load(IMAGE_URL)
+                .into(IMG1);
+        ImageView IMG2 = findViewById(R.id.imageView3);
+
+        Glide.with(this)
+                .load(AVATAR_URL)
+                .into(IMG2);
+
+        TextView Title = findViewById(R.id.textView3);
+        TextView date = findViewById(R.id.textView2);
+        TextView author = findViewById(R.id.textView4);
+        RatingBar stars = findViewById(R.id.ratingBar);
+        TextView textStar = findViewById(R.id.textView5);
+        TextView views = findViewById(R.id.textView6);
+        TextView caption = findViewById(R.id.textView7);
+        // IMG1.setImageResource(R.drawable.sydney_image);
+        // IMG2.setImageResource(R.drawable.avatar);
+        Title.setText("G'day from Sydney");
+        date.setText("12th February 2023");
+        author.setText("Grayson Wells");
+        stars.setNumStars(5);
+        stars.setRating((float)4.4);
+        textStar.setText("4.4");
+        views.setText("(8529 views)");
+        caption.setText("Australia is one of the most popular travel destinations in the world.");
+
+
     }
 }
